c243e6820370cff2f820286114f7d2bf92e94fe5
s3ponia <slava.shlyaga@gmail.com>
2020-05-19 18:05:44 +0300

    fix delims. add save to files

--- RussianFoodComfRecipe.py
+++ RussianFoodComfRecipe.py
@@ -1,8 +1,17 @@
-import json
-from typing import List
+﻿import json
+from typing import List, Tuple
 from Ingredient import Ingredient
 from bs4 import BeautifulSoup
 from Recipe import Recipe
+import re
+
+
+pattern = re.compile('(.+){\-—}(.+)')
+def splitIngr(ingr: str) -> Tuple[str, str]:
+    groups = pattern.match(ingr)
+    if groups is None:
+        return (ingr, '')
+    return (groups.group(0).strip(), groups.group(1).strip())
 
 
 def decode_russian_food_com(html_str: str) -> Recipe:
@@ -10,9 +19,12 @@ def decode_russian_food_com(html_str: str) -> Recipe:
         name = soup.find("h1", {"class": "title"}).get_text().strip()
         ingredients1 = soup.find_all("tr", {"class": "ingr_tr_0"})
         ingredients2 = soup.find_all("tr", {"class": "ingr_tr_1"})
-        ingredients = [Ingredient(ingredient.get_text().strip().split(' -')) for ingredient in
+        ingredients = [Ingredient(splitIngr(ingredient.get_text())) for ingredient in
                             ingredients1 + ingredients2]
+        how_dirs = [recipe.get_text().strip() for recipe in soup.find(id='how').find_all('p') if len(recipe.get_text()) > 0 and  recipe.get_text()[0].isdigit()]
         directions = [recipe.get_text().strip() for recipe in soup.find_all("div", {"class": "step_n"})]
+        if len(how_dirs) > 0:
+            directions = how_dirs
         description = recipe = \
             soup.find_all("table", {"class": "recipe_new"})[0].find_all("td", {"class": "padding_l padding_r"})[
                 0].find(
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import os
+from os import listdir
+from os.path import isfile, join
+import sys
 import time
 from RussianFoodComfRecipe import decode_russian_food_com
 
@@ -18,30 +21,34 @@ def send(json_str: str):
                'Content-Encoding': 'utf-8'}
     answer = requests.post(url, data=json_str, headers=headers)
     print(answer)
-    response = answer.json()
-    print(response)
 
+def send_all():
+    out_dir = './out'
+    onlyfiles = [f for f in listdir(out_dir) if isfile(join(out_dir, f))]
+    for filename in onlyfiles:
+        with open(join(out_dir, filename), 'r') as f:
+            send(f.read())
 
-s = requests.Session()
-recipes_quantity = 100
-counter = 1
+id_list = [156189, 152510, 134935, 121118, 123433, 156089, 137206, 116665, 102711, 121844,
+            152213, 58655, 126678, 121626, 130693, 140838, 122737, 134924, 141692, 123559, 136031, 117690, 142942,
+            132454, 131921, 127181, 132593, 137701, 107064, 146941, 142047, 147963, 131921, 141929, 132716]
 if not os.path.exists('./out'):
     os.mkdir('./out')
-while recipes_quantity > 0:
-    try:
-        print('.', end='')
-        time.sleep(5)
-        bytes_url = s.get("https://www.russianfood.com/recipes/recipe.php?rid={0}".format(counter))
-        counter += 1
-        html_doc = bytes_url.text
+for recipe_id in id_list:
+    if os.path.exists('./out/out_recipe{0}.json'.format(recipe_id)):
+        continue
+    time.sleep(5)
+    print('.', end='')
+    sys.stdout.flush()
+    bytes_url = requests.get("https://www.russianfood.com/recipes/recipe.php?rid={0}".format(recipe_id))
+    html_doc = bytes_url.text
+    print(recipe_id,'\n')
 
-        soup = BeautifulSoup(html_doc, 'html.parser')
+    soup = BeautifulSoup(html_doc, 'html.parser')
 
-        t = decode_russian_food_com(html_doc)
+    t = decode_russian_food_com(html_doc)
 
-        with open('./out/out_recipe{0}.json'.format(recipes_quantity), 'w') as f:
-            f.write(t.to_json())
-            recipes_quantity -= 1
-            print('\n', recipes_quantity)
-    except Exception:
-        pass
+    with open('./out/out_recipe{0}.json'.format(recipe_id), 'w') as f:
+        f.write(t.to_json())
+
+
